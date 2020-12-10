3d187b3242bff3043139c16c8a72368379b100a0
DenisDaniel707 <48672958+DenisDaniel707@users.noreply.github.com>
2020-12-10 23:42:58 +0200

    Create Pasare.hpp

---
+++ IEP-Lucrare10/src/Pasare.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+#ifndef IEP_PASARE_HPP
+#define IEP_PASARE_HPP
+
+class Pasare {
+	public:
+	    virtual void zboara() = 0;	//initialise virtual functions
+	    virtual void sunet() = 0;
+};
+
+#endif
